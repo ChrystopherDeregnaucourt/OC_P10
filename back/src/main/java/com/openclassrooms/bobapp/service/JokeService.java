ac28ccbc9df1b56732140c34d3f17829f69320ab
Chrys <deregnaucourt.chrystopher@hotmail.fr>
2026-01-12 10:10:22 +0100

    Fix : use SecureRandom

--- back/src/main/java/com/openclassrooms/bobapp/service/JokeService.java
+++ back/src/main/java/com/openclassrooms/bobapp/service/JokeService.java
@@ -1,7 +1,7 @@
 package com.openclassrooms.bobapp.service;
 
+import java.security.SecureRandom;
 import java.util.List;
-import java.util.Random;
 
 import org.springframework.stereotype.Service;
 
@@ -11,7 +11,7 @@ import com.openclassrooms.bobapp.model.Joke;
 @Service
 public class JokeService
 {
-    private static final Random RANDOM = new Random();
+    private static final SecureRandom RANDOM = new SecureRandom();
     private final JsonReader jsonReader;
 
     JokeService(JsonReader jsonReader)
